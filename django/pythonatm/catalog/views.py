b1652305d9b02a2bfb933927b93b88d6df10c605
LeVarS <small.levar98@gmail.com>
2019-10-31 17:20:09 -0400

    A working start to the user page

--- django/pythonatm/catalog/views.py
+++ django/pythonatm/catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from catalog.models import Account, Card, ATMachine, ATMachineRefill, Transaction
 
 # Create your views here.
+
+def index(request):
+    """ View function for home page of site. """
+
+    # Generate counts of some fo the main objects
+    num_accounts = Account.objects.count()    # same as "Account.objects.all().count()"
+    num_cards = Card.objects.count()
+    num_atms = ATMachine.objects.count()
+    num_transactions = Transaction.objects.count()
+
+    # Available book (status = 'a')
+    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'num_accounts': num_accounts,
+        'num_cards': num_cards,
+        'num_atms': num_atms,
+        'num_transactions': num_transactions,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
