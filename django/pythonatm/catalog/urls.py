a5f2c8b830bb49a3cdac40ee20d7904da5487a26
LeVarS <small.levar98@gmail.com>
2019-11-01 00:10:45 -0400

    Added logging-in and out functionality as well as the ability to change passwords

--- django/pythonatm/catalog/urls.py
+++ django/pythonatm/catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', views.AccountListView.as_view(), name='accounts'),
-     path('account/<int:pk>', views.AccountDetailView.as_view(), name='account-detail'),
+    path('account/<int:pk>', views.AccountDetailView.as_view(), name='account-detail'),
 ]
